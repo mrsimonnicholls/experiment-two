158eece69a3fff2c15b86245222347997918d082
Simon Nicholls <simon.nicholls@mediadesignschool.com>
2025-02-20 08:40:51 +1300

    added drawRectangle function

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
 function setup() {
   createCanvas(640, 480);
+  angleMode(DEGREES);
+  translate(width / 2, height / 2);
+  noLoop();
 }
 
 function draw() {
   background(220);
-  
+  drawRectangle(width/2, height/2, 100);
+  drawRectangle(300,300,50,45);
+}
+
+function drawRectangle(x,y,w,r = 0) {
+  rotate(r);
+  rect(x,y,w);
+  rotate(-1 * r );
 }
